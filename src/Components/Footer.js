8f12ab78f354f5bb8dc7354a1745e65fe368d516
“liton-ahmed” <litonahmed066@gmail.com>
2023-08-14 00:14:25 +0600

    footer update

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,58 +1,63 @@
-import React from 'react';
-import './Footer.css';
-import image from '../images/icons/logo.png'
+import React from "react";
+import "./Footer.css";
+import image from "../images/icons/logo.png";
 
 export default function Footer() {
-    return (
-        <div className='commonDiv'>
-            <div className='footer'>
-
-                <div className='imgDiv'>
-                    <img style={{ height: '40px' }} src={image} />
-                </div>
-
-                <div className='detailsDiv'>
-                  <div className='info'>
-                  <div>
-                        <ul>
-                            <li><a href='#'>About Online Food</a></li>
-                            <li><a href='#'>Read Our Blog</a></li>
-                            <li><a href='#'>Sign Up to Deliver</a></li>
-                            <li><a href='#'>Add your Restaurant</a></li>
-
-                        </ul>
-                    </div>
-
-                <div>
-                    <ul>
-                        <li><a href='#'>Get help</a></li>
-                        <li><a href='#'>Read FAQS</a></li>
-                        <li><a href='#'>View All cities</a></li>
-                        <li><a href='#'>Restaurant near me</a></li>
-
-                    </ul>
-
-                </div>
-                  </div>
-                </div>
-
-            </div>
-
-            <div className='lastDiv'>
-                <div className='copy'>
-                    <p>copyright@ 2023 Online Food </p>
-                </div>
-                <div className='privacy'>
-                    <h6> Privacy Policy</h6>
-                </div>
-                <div className='terms'>
-                    <h6>Terms Of Use</h6>
-                </div>
-                <div className='price'>
-                    <h6>Pricing</h6>
-                </div>
-
-            </div>
+  return (
+    <div className="commonDiv">
+      <div className="footer">
+        <div className="imgDiv">
+          <img style={{ height: "40px" }} src={image} />
         </div>
-    )
+
+        <div>
+          <ul>
+            <li>
+              <a href="#">About Online Food</a>
+            </li>
+            <li>
+              <a href="#">Read Our Blog</a>
+            </li>
+            <li>
+              <a href="#">Sign Up to Deliver</a>
+            </li>
+            <li>
+              <a href="#">Add your Restaurant</a>
+            </li>
+          </ul>
+        </div>
+
+        <div>
+          <ul>
+            <li>
+              <a href="#">Get help</a>
+            </li>
+            <li>
+              <a href="#">Read FAQS</a>
+            </li>
+            <li>
+              <a href="#">View All cities</a>
+            </li>
+            <li>
+              <a href="#">Restaurant near me</a>
+            </li>
+          </ul>
+        </div>
+
+        <div className="privacy com">
+          <a href="#"> Privacy Policy</a>
+        </div>
+        <div className="terms com">
+          <a href="#">Terms Of Use</a>
+        </div>
+        <div className="price com">
+          <a href="#">Pricing</a>
+        </div>
+      </div>
+
+      <div className="copy">
+        <p>copyright@ 2023 Online Food </p>
+      </div>
+    </div>
+  );
 }
--- src/Components/FooterCard.js
+++ src/Components/FooterCard.js
@@ -1,24 +1,20 @@
-import React from 'react';
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
-import './FooterCard.css';
+import React from "react";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import "./FooterCard.css";
 
 export default function FooterCard(props) {
   const { service, img, description } = props.data;
   return (
-    <div className='container service'>
-
-      <Card className=' border-0' style={{ width: '16rem' }}>
-        <Card.Img variant="top" style={{ height: '15rem' }} src={img} />
+    <div className="container service">
+      <Card className=" border-0" style={{ width: "16rem" }}>
+        <Card.Img variant="top" style={{ height: "15rem" }} src={img} />
         <Card.Body>
           <Card.Title>{service}</Card.Title>
-          <Card.Text>
-            {description}
-          </Card.Text>
+          <Card.Text>{description}</Card.Text>
           <Button variant="primary">see more</Button>
         </Card.Body>
       </Card>
-
     </div>
-  )
+  );
 }
